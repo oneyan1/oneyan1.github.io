5c54908620c23a12884006cab151c6dcf88eb02d
oneyan1 <yangrodno@gmail.com>
2020-03-08 14:53:24 +0100

    added crypto task

---
+++ portfolio/crypto/precache-manifest.83e3a73dd64e0cbfd9dc85a7f39c3400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b2949007b985627da6dded22b9bd48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3726bc3a8326ebc2f6b0",
+    "url": "/static/css/main.3aab3855.chunk.css"
+  },
+  {
+    "revision": "eff85bc4366a50d0f448",
+    "url": "/static/js/2.aca5bd3e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.aca5bd3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3726bc3a8326ebc2f6b0",
+    "url": "/static/js/main.97d96b13.chunk.js"
+  },
+  {
+    "revision": "bed4ec944f2d665960fc",
+    "url": "/static/js/runtime-main.aae5eb29.js"
+  }
+]);
